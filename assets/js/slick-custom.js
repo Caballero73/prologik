627d0e6eb5fc0d6ed95c9988969d658980213c4b
Enrique <comisionado.falcon@gmail.com>
2018-03-13 16:16:08 -0400

    Vista CASOS DE EXITO

--- assets/js/slick-custom.js
+++ assets/js/slick-custom.js
@@ -1,5 +1,5 @@
-//Carrusel Aboutus
 $(document).ready(function(){
+  //Carrusel Aboutus
   $('.aboutus-carrusel').slick({
     dots: true,
     infinite: true,
@@ -8,120 +8,74 @@ $(document).ready(function(){
     arrows: false,
     adaptiveHeight: true,
 	});
-});
 
-//Carrusel Aboutus Responsive
-$('.aboutus-carrusel2').slick({
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  arrows: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
+  //Carrusel Aboutus Responsive
+  $('.aboutus-carrusel2').slick({
+    dots: true,
+    infinite: true,
+    speed: 1000,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+    ]
+  });
 
-//Carrusel Success
-$(document).ready(function(){
+  //Carrusel Aboutus Responsive
   $('.success-carrusel').slick({
     dots: true,
     infinite: true,
     speed: 1000,
-    slidesToShow: 1,
     arrows: false,
-    adaptiveHeight: true,
-  });
-});
-
-//Carruseles Casos de exito
-$('.success-event1').slick({
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  arrows: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
       }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
-
-$('.success-event2').slick({
-  dots: true,
-  infinite: true,
-  speed: 1000,
-  arrows: false,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+    ]
+  })
 });
